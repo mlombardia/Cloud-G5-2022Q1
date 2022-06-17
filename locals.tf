be22792deb51ad31a306143c3137d78700a9114d
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2022-06-17 18:22:33 -0300

    feat: cloudfront

--- locals.tf
+++ locals.tf
@@ -1,5 +1,7 @@
 locals {
   bucket_name = "itba-tp-podcasts-bucket2"
   path        = "./resources"
-
+  cloudfront_bucket_name = "itba-tp-podcasts-cloudfront-bucket"
+  s3_origin_id = "podcastS3Origin"
+  
 }
