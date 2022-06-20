c8e47994f649ffcb03b2617c511bf3c79dbfd301
mariano <marperez@itba.edu.ar>
2022-06-19 21:40:12 -0300

    user group  & identity pool

--- cognito.tf
+++ cognito.tf
@@ -6,3 +6,23 @@ resource "aws_cognito_user" "user1" {
   user_pool_id = aws_cognito_user_pool.pool.id
   username     = "user1"
 }
+
+
+resource "aws_cognito_user_group" "main" {
+  name         = "user-group"
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
+
+resource "aws_cognito_user_group" "user2" {
+  name         = "user2-group"
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
+
+
+resource "aws_cognito_identity_pool" "main" {
+  identity_pool_name               = "identity pool"
+  allow_unauthenticated_identities = false
+  allow_classic_flow               = false
+
+  
+}
